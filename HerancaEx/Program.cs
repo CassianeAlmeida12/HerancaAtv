2b8a7b326e665ce1685413bce080b0aaae53c6e3
Cassiane Almeida <cassiane.almeida@estudante.ifro.edu.br>
2023-09-18 16:39:20 -0400

    quarto commit

--- HerancaEx/Program.cs
+++ HerancaEx/Program.cs
@@ -22,6 +22,30 @@ public class Programa
                 {
                     Console.Write("Digite o número da conta: ");
                     c.NConta = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Digite o nome da sua agência: ");
+                    c.Agencia = Console.ReadLine();
+                    Console.Write("Digite o nome do Titual da conta: ");
+                    c.Titular = Console.ReadLine();
+                    Console.Write("Digite o seu saldo atual: ");
+                    c.Saldo = Convert.ToDouble(Console.ReadLine());
+
+                    Console.WriteLine("\nQual das operação abaixo você gostaria de realizar? ");
+                    Console.WriteLine("1 - Sacar");
+                    Console.WriteLine("2 - Depositar");
+                    int escolha2 = Convert.ToInt32(Console.ReadLine());
+
+                    if( escolha2 == 1)
+                    {
+                        Console.Write("\nDigite o valor que você quer sacar: ");
+                        double valor = Convert.ToDouble(Console.ReadLine());
+                        c.Sacar(valor);
+                    }
+                    else if(escolha2 == 2)
+                    {
+                        Console.Write("Digite o valor que você quer depositar: ");
+                        double valor = Convert.ToDouble(Console.ReadLine());
+                        c.Depositar(valor);
+                    }
                 }
             }
             catch(Exception ex)
